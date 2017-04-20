73a00d19708032dc11996649fe1c8d1b485f596e
Adel Attia <attia3adel@gmail.com>
2017-04-20 18:08:34 +0100

    example arrow functions

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -7,12 +7,9 @@ const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?app
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
-    console.log(encodedLocation);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-    console.log(requestUrl);
 
     return axios.get(requestUrl).then(function (resp) {
-      console.log(resp);
       if (resp.data.cod && resp.data.message) {
         throw new Error(resp.data.message);
       } else {
