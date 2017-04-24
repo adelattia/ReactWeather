fd88d4fb6d12ff8a1261b680affaa0c52c3a2d53
Adel Attia <attia3adel@gmail.com>
2017-04-24 15:13:26 +0100

    style the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,14 @@ var React = require('react');
 
 var About = () => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-centered">About</h1>
+      <p>
+        This is a weather application built using <a href="https://facebook.github.io/react" target="_blank">React</a>, following <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/" target="_blank">"The complete React Web App Developer Course."</a>
+      <br/>
+      I used <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> to search for weather data by city name.
+      </p>
+    </div>
   );
 };
 
